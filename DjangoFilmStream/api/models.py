8e31115835de98d4234ce0633e5ce4d4342903e4
Pipe <pipegarcia1918@hotmail.com>
2025-04-12 22:39:59 -0500

    feat: create models for movies

--- DjangoFilmStream/api/models.py
+++ DjangoFilmStream/api/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True)
+
+    video_url = models.URLField(blank=True, null=True)
+    trailer_url = models.URLField(blank=True, null=True)
+    cover_image_url = models.URLField(blank=True, null=True)
+
+    is_public = models.BooleanField(default=True)
+    duration = models.PositiveIntegerField(help_text="Duration in seconds", blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
